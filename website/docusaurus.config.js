41aaaf5bbd46295488bc2c41a8f4ce5a78a6f812
Piotr PG Gajek <piotr.gajek95@gmail.com>
2023-03-14 17:59:44 +0100

    ignore links

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'Beyond The Cloud', // Usually your GitHub org/user name.
   projectName: 'query-selector', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
